39a72025d29ed6a772093226770702f1efe8d543
HubPie <pieniazekh@gmail.com>
2024-02-04 23:02:55 +0100

    models

---
+++ LocalGems/Models/CustomMarker.cs
@@ -0,0 +1,18 @@
+﻿using Syncfusion.Maui.Maps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalGems.Models
+{
+    public class CustomMarker : MapMarker
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public CustomMarker() { }
+
+    }
+}
